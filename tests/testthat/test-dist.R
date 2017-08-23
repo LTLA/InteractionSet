848f08042bf5e62993820414b5ea7fad6a4d667f
Aaron Lun <alun@wehi.edu.au>
2017-08-23 11:50:14 +0100

    Compartmentalized tests for distance, GR methods, linearize.

--- tests/testthat/test-dist.R
+++ tests/testthat/test-dist.R
@@ -1,4 +1,5 @@
 # Tests the distance calculation methods for an InteractionSet.
+# library(InteractionSet); library(testthat); source("test-dist.R")
 
 set.seed(100)
 N <- 50
@@ -14,32 +15,32 @@ counts <- matrix(rpois(Np*Nlibs, lambda=10), ncol=Nlibs)
 colnames(counts) <- seq_len(Nlibs)
 x <- InteractionSet(counts, GInteractions(all.anchor1, all.anchor2, all.regions))
 
-a1 <- all.regions[all.anchor1]
-a2 <- all.regions[all.anchor2]
-swap <- a1 < a2
-temp <- a2[swap]
-a2[swap] <- a1[swap]
-a1[swap] <- temp
-
-is.intra <- intrachr(x)
-expect_that(is.intra, is_identical_to(as.logical(seqnames(a1)==seqnames(a2))))
-expect_that(is.intra, is_identical_to(pairdist(x, type="intra")))
-expect_that(pairdist(x), is_identical_to(ifelse(is.intra, as.integer(abs(start(a1)+end(a1)-start(a2)-end(a2))/2L), as.integer(NA)))) # Don't use 'mid', it does its own rounding.
-expect_that(pairdist(x,type="gap"), is_identical_to(ifelse(is.intra, pmax(start(a1), start(a2)) - pmin(end(a1), end(a2)) -1L, as.integer(NA)))) 
-expect_that(pairdist(x,type="span"), is_identical_to(ifelse(is.intra, pmax(end(a1), end(a2)) - pmin(start(a1), start(a2)) +1L, as.integer(NA)))) 
-
-ax1 <- anchors(x, type="first", id=TRUE)
-ax2 <- anchors(x, type="second", id=TRUE)
-expect_that(pairdist(x, type="diag"), is_identical_to(ifelse(is.intra, pmax(ax1, ax2) - pmin(ax1, ax2), as.integer(NA))))
-
-# What happens with empty inputs?
-
-expect_that(pairdist(x[0,]), is_identical_to(integer(0)))
-expect_that(pairdist(x[0,], type="intra"), is_identical_to(logical(0)))
-expect_that(pairdist(x[!is.intra,]), is_identical_to(rep(as.integer(NA), sum(!is.intra))))
+test_that("Paired distances are correct for IS objects", {
+    a1 <- all.regions[all.anchor1]
+    a2 <- all.regions[all.anchor2]
+    swap <- a1 < a2
+    temp <- a2[swap]
+    a2[swap] <- a1[swap]
+    a1[swap] <- temp
+    
+    is.intra <- intrachr(x)
+    expect_that(is.intra, is_identical_to(as.logical(seqnames(a1)==seqnames(a2))))
+    expect_that(is.intra, is_identical_to(pairdist(x, type="intra")))
+    expect_that(pairdist(x), is_identical_to(ifelse(is.intra, as.integer(abs(start(a1)+end(a1)-start(a2)-end(a2))/2L), as.integer(NA)))) # Don't use 'mid', it does its own rounding.
+    expect_that(pairdist(x,type="gap"), is_identical_to(ifelse(is.intra, pmax(start(a1), start(a2)) - pmin(end(a1), end(a2)) -1L, as.integer(NA)))) 
+    expect_that(pairdist(x,type="span"), is_identical_to(ifelse(is.intra, pmax(end(a1), end(a2)) - pmin(start(a1), start(a2)) +1L, as.integer(NA)))) 
+    
+    ax1 <- anchors(x, type="first", id=TRUE)
+    ax2 <- anchors(x, type="second", id=TRUE)
+    expect_that(pairdist(x, type="diag"), is_identical_to(ifelse(is.intra, pmax(ax1, ax2) - pmin(ax1, ax2), as.integer(NA))))
+    
+    # What happens with empty inputs?
+    expect_that(pairdist(x[0,]), is_identical_to(integer(0)))
+    expect_that(pairdist(x[0,], type="intra"), is_identical_to(logical(0)))
+    expect_that(pairdist(x[!is.intra,]), is_identical_to(rep(as.integer(NA), sum(!is.intra))))
+})
 
 ####################################################
-# Testing distance calculation for a ContactMatrix.
 
 set.seed(101)
 Nr <- 100
@@ -49,42 +50,41 @@ all.anchor2 <- sample(N, Nc, replace=TRUE)
 counts <- matrix(rpois(Nr*Nc, lambda=10), Nr, Nc)
 x <- ContactMatrix(counts, all.anchor1, all.anchor2, all.regions)
 
-a1 <- anchors(x, type="row")
-a2 <- anchors(x, type="column")
-is.intra <- intrachr(x) 
-expect_that(is.intra, is_identical_to(matrix(outer(as.character(seqnames(a1)), as.character(seqnames(a2)), "=="), nrow=Nr, ncol=Nc)))
-expect_that(is.intra, is_identical_to(pairdist(x, type="intra")))
-
-ref <- abs(outer(start(a1)+end(a1), start(a2)+end(a2), "-"))
-ref <- ref/2L
-ref[!is.intra] <- NA
-storage.mode(ref) <- "integer"
-expect_that(pairdist(x), is_identical_to(ref))
-                                                
-ref <- abs(outer(anchors(x, type="row", id=TRUE), anchors(x, type="column", id=TRUE), "-"))
-ref[!is.intra] <- NA
-storage.mode(ref) <- "integer"
-expect_that(pairdist(x, type="diag"), is_identical_to(ref))
-
-as1 <- start(a1)
-as2 <- start(a2)
-ae1 <- end(a1)
-ae2 <- end(a2)
-ref <- outer(as1, as2, pmax) - outer(ae1, ae2, pmin) - 1L
-ref[!is.intra] <- NA                    
-expect_that(pairdist(x,type="gap"), is_identical_to(ref))
-
-ref <- outer(ae1, ae2, pmax) - outer(as1, as2, pmin) + 1L
-ref[!is.intra] <- NA                    
-expect_that(pairdist(x,type="span"), is_identical_to(ref))
-
-# Don't need to worry about NA's in 'x@matrix', they don't affect the calculation.
-# Still checking silly inputs, though:
-
-expect_that(pairdist(x[0,]), is_identical_to(matrix(0L, 0, Nc)))
-expect_that(pairdist(x[,0], type="intra"), is_identical_to(matrix(FALSE, Nr, 0)))
-is.ra <- which(seqnames(anchors(x, type="row"))=="chrA")
-is.cb <- which(seqnames(anchors(x, type="column"))=="chrB")
-expect_that(pairdist(x[is.ra,is.cb]), is_identical_to(matrix(as.integer(NA), length(is.ra), length(is.cb))))
-
-
+test_that("Paired distances are correct for CM objects", {
+    a1 <- anchors(x, type="row")
+    a2 <- anchors(x, type="column")
+    is.intra <- intrachr(x) 
+    expect_that(is.intra, is_identical_to(matrix(outer(as.character(seqnames(a1)), as.character(seqnames(a2)), "=="), nrow=Nr, ncol=Nc)))
+    expect_that(is.intra, is_identical_to(pairdist(x, type="intra")))
+    
+    ref <- abs(outer(start(a1)+end(a1), start(a2)+end(a2), "-"))
+    ref <- ref/2L
+    ref[!is.intra] <- NA
+    storage.mode(ref) <- "integer"
+    expect_that(pairdist(x), is_identical_to(ref))
+                                                    
+    ref <- abs(outer(anchors(x, type="row", id=TRUE), anchors(x, type="column", id=TRUE), "-"))
+    ref[!is.intra] <- NA
+    storage.mode(ref) <- "integer"
+    expect_that(pairdist(x, type="diag"), is_identical_to(ref))
+    
+    as1 <- start(a1)
+    as2 <- start(a2)
+    ae1 <- end(a1)
+    ae2 <- end(a2)
+    ref <- outer(as1, as2, pmax) - outer(ae1, ae2, pmin) - 1L
+    ref[!is.intra] <- NA                    
+    expect_that(pairdist(x,type="gap"), is_identical_to(ref))
+    
+    ref <- outer(ae1, ae2, pmax) - outer(as1, as2, pmin) + 1L
+    ref[!is.intra] <- NA                    
+    expect_that(pairdist(x,type="span"), is_identical_to(ref))
+    
+    # Don't need to worry about NA's in 'x@matrix', they don't affect the calculation.
+    # Still checking silly inputs, though:   
+    expect_that(pairdist(x[0,]), is_identical_to(matrix(0L, 0, Nc)))
+    expect_that(pairdist(x[,0], type="intra"), is_identical_to(matrix(FALSE, Nr, 0)))
+    is.ra <- which(seqnames(anchors(x, type="row"))=="chrA")
+    is.cb <- which(seqnames(anchors(x, type="column"))=="chrB")
+    expect_that(pairdist(x[is.ra,is.cb]), is_identical_to(matrix(as.integer(NA), length(is.ra), length(is.cb))))
+})
--- tests/testthat/test-linear.R
+++ tests/testthat/test-linear.R
@@ -1,4 +1,5 @@
 # Testing the linearize function for InteractionSet objects.
+# library(InteractionSet); library(testthat); source("test-linear.R")
 
 set.seed(200)
 N <- 50
@@ -16,21 +17,22 @@ counts <- matrix(rpois(Np*Nlibs, lambda=10), ncol=Nlibs)
 colnames(counts) <- seq_len(Nlibs)
 x <- InteractionSet(counts, GInteractions(all.anchor1, all.anchor2, all.regions))
 
-o <- order(all.regions)
-new.regions <- all.regions[o]
-new.pos <- integer(length(o))
-new.pos[o] <- seq_along(new.pos)
-new.anchor1 <- new.pos[all.anchor1]
-new.anchor2 <- new.pos[all.anchor2]
+####################################
 
-# Running through all possibilities:
+test_that("conversion via linearization is working", {
+    o <- order(all.regions)
+    new.regions <- all.regions[o]
+    new.pos <- integer(length(o))
+    new.pos[o] <- seq_along(new.pos)
+    new.anchor1 <- new.pos[all.anchor1]
+    new.anchor2 <- new.pos[all.anchor2]
 
-for (interest in seq_len(N)) {
-    cur.reg <- regions(x)[interest]
-    out <- linearize(x, interest)
-    chosen <- new.anchor1==interest | new.anchor2==interest
-    expect_that(assay(out), is_identical_to(assay(x[chosen,])))
-    expect_output(show(out), sprintf("class: RangedSummarizedExperiment 
+    for (interest in seq_len(N)) {
+        cur.reg <- regions(x)[interest]
+        out <- linearize(x, interest)
+        chosen <- new.anchor1==interest | new.anchor2==interest
+        expect_identical(assay(out), assay(x[chosen,]))
+        expect_output(show(out), sprintf("class: RangedSummarizedExperiment 
 dim: %i 4 
 metadata(0):
 assays(1): ''
@@ -38,73 +40,86 @@ rownames: NULL
 rowData names(0):
 colnames(4): 1 2 3 4
 colData names(0):", sum(chosen)), fixed=TRUE)
+    
+        new.ranges <- anchors(x, type="first")
+        not1 <- new.ranges!=cur.reg
+        new.ranges[!not1] <- anchors(x, type="second")[!not1]
+        new.ranges <- new.ranges[chosen]
+        expect_identical(rowRanges(out), new.ranges)
+    
+        # Comparing with equivalent GRanges method.
+        out2 <- linearize(x, cur.reg, type="equal")
+        expect_equal(out, out2)
+    
+        # Also checking with GInteractions methods.
+        out3 <- linearize(interactions(x), cur.reg, type="equal")
+        expect_identical(out3, rowRanges(out2))
+        out3 <- linearize(interactions(x), interest)
+        expect_identical(out3, rowRanges(out2))
+    }
+})
 
-    new.ranges <- anchors(x, type="first")
-    not1 <- new.ranges!=cur.reg
-    new.ranges[!not1] <- anchors(x, type="second")[!not1]
-    new.ranges <- new.ranges[chosen]
-    expect_that(rowRanges(out), is_identical_to(new.ranges))
+test_that("linearization behaves correctly with silly inputs", {
+    expect_identical(nrow(linearize(x, 0)), 0L)
+    expect_identical(nrow(linearize(x[0,], 1)), 0L)
+    expect_identical(nrow(suppressWarnings(linearize(x, GRanges("chrC", IRanges(1,1))))), 0L)
+})
 
-    # Comparing with equivalent GRanges method.
-    out2 <- linearize(x, cur.reg, type="equal")
-    expect_that(out, equals(out2))
+####################################
 
-    # Also checking with GInteractions methods.
-    out3 <- linearize(interactions(x), cur.reg, type="equal")
-    expect_identical(out3, rowRanges(out2))
-    out3 <- linearize(interactions(x), interest)
-    expect_identical(out3, rowRanges(out2))
-}
+test_that("linearization works with multiple overlaps", {
+    lenA <- max(end(regions(x)[seqnames(regions(x))=="chrA"]))
+    x2 <- x
+    mcols(x2)$Index <- seq_len(nrow(x2))
+    out <- linearize(x2, GRanges("chrA", IRanges(1, lenA)))
+    all.a <- anchors(x2[mcols(out)$Index])
+    mcols(out)$Index <- NULL
+   
+    # Testing for proper merging of internal ranges. 
+    is.same <- as.logical(seqnames(all.a$first)==seqnames(all.a$second))
+    expect_identical(pmin(start(all.a$first), start(all.a$second))[is.same], start(rowRanges(out))[is.same])
+    expect_identical(pmax(end(all.a$first), end(all.a$second))[is.same], end(rowRanges(out))[is.same])
+    expect_identical(seqnames(all.a$first)[is.same], seqnames(rowRanges(out))[is.same])
+    
+    is.B1 <- as.logical(seqnames(all.a$first)=="chrB")
+    expect_identical(all.a$first[is.B1], rowRanges(out)[is.B1])
+    is.B2 <- as.logical(seqnames(all.a$second)=="chrB")
+    expect_identical(all.a$second[is.B2], rowRanges(out)[is.B2])
+    
+    lenB <- max(end(regions(x)[seqnames(regions(x))=="chrB"]))
+    expect_error(linearize(x2, GRanges(c("chrA", "chrB"), IRanges(1, c(lenA, lenB)))), "multi-chromosome sets of 'ref' are not supported")
+})
 
-# What happens with silly inputs?
-
-expect_that(nrow(linearize(x, 0)), is_identical_to(0L))
-expect_that(nrow(linearize(x[0,], 1)), is_identical_to(0L))
-expect_that(nrow(suppressWarnings(linearize(x, GRanges("chrC", IRanges(1,1))))), is_identical_to(0L))
-
-# Testing what happens when there are multiple overlaps.
-
-lenA <- max(end(regions(x)[seqnames(regions(x))=="chrA"]))
-x2 <- x
-mcols(x2)$Index <- seq_len(nrow(x2))
-out <- linearize(x2, GRanges("chrA", IRanges(1, lenA)))
-all.a <- anchors(x2[mcols(out)$Index])
-mcols(out)$Index <- NULL
-
-is.same <- as.logical(seqnames(all.a$first)==seqnames(all.a$second))
-expect_identical(pmin(start(all.a$first), start(all.a$second))[is.same], start(rowRanges(out))[is.same])
-expect_identical(pmax(end(all.a$first), end(all.a$second))[is.same], end(rowRanges(out))[is.same])
-expect_identical(seqnames(all.a$first)[is.same], seqnames(rowRanges(out))[is.same])
-
-is.B1 <- as.logical(seqnames(all.a$first)=="chrB")
-expect_identical(all.a$first[is.B1], rowRanges(out)[is.B1])
-is.B2 <- as.logical(seqnames(all.a$second)=="chrB")
-expect_identical(all.a$second[is.B2], rowRanges(out)[is.B2])
-
-lenB <- max(end(regions(x)[seqnames(regions(x))=="chrB"]))
-expect_error(linearize(x2, GRanges(c("chrA", "chrB"), IRanges(1, c(lenA, lenB)))), "multi-chromosome sets of 'ref' are not supported")
-
-# Repeating with internal=FALSE.
-
-out <- linearize(x2, GRanges("chrA", IRanges(1, lenA)), internal=FALSE)
-all.a <- anchors(x2[mcols(out)$Index])
-mcols(out)$Index <- NULL
-
-is.diff <- as.logical(seqnames(all.a$first)!=seqnames(all.a$second))
-expect_true(all(is.diff))
-is.B1 <- as.logical(seqnames(all.a$first)=="chrB")
-expect_identical(all.a$first[is.B1], rowRanges(out)[is.B1])
-is.B2 <- as.logical(seqnames(all.a$second)=="chrB")
-expect_identical(all.a$second[is.B2], rowRanges(out)[is.B2])
+test_that("linearization correctly ignores internal interactions", {
+    lenA <- max(end(regions(x)[seqnames(regions(x))=="chrA"]))
+    x2 <- x
+    mcols(x2)$Index <- seq_len(nrow(x2))
+    out <- linearize(x2, GRanges("chrA", IRanges(1, lenA)), internal=FALSE)
+    all.a <- anchors(x2[mcols(out)$Index])
+    mcols(out)$Index <- NULL
+    
+    is.diff <- as.logical(seqnames(all.a$first)!=seqnames(all.a$second))
+    expect_true(all(is.diff))
+    is.B1 <- as.logical(seqnames(all.a$first)=="chrB")
+    expect_identical(all.a$first[is.B1], rowRanges(out)[is.B1])
+    is.B2 <- as.logical(seqnames(all.a$second)=="chrB")
+    expect_identical(all.a$second[is.B2], rowRanges(out)[is.B2])
+})
 
 # Testing what happens with region and interaction-specific metadata.
 
 set.seed(100)
-out <- linearize(x, 1)
-mcols(x) <- DataFrame(Blah=runif(nrow(x)), Index=seq_len(nrow(x)))
-mcols(regions(x)) <- DataFrame(Foo=runif(length(regions(x))))
+test_that("linearization preserves metadata", {
+    out <- linearize(x, 1)
+    mcols(x) <- DataFrame(Blah=runif(nrow(x)), Index=seq_len(nrow(x)))
+    mcols(regions(x)) <- DataFrame(Foo=runif(length(regions(x))))
 
-out2 <- linearize(x, 1)
-expect_identical(colnames(mcols(out2)), c("Foo", "Blah", "Index"))
-expect_identical(mcols(out2)$Blah, mcols(x)$Blah[mcols(out2)$Index])
+    out2 <- linearize(x, 1)
+    expect_identical(colnames(mcols(out2)), c("Foo", "Blah", "Index"))
+    expect_identical(mcols(out2)$Blah, mcols(x)$Blah[mcols(out2)$Index])
 
+    a1 <- anchors(x, type="first", id=TRUE)
+    a2 <- anchors(x, type="second", id=TRUE)
+    index <- ifelse(a1!=1L, a1, a2)[mcols(out2)$Index]
+    expect_identical(mcols(out2)$Foo, mcols(regions(x))$Foo[index])
+})
