36303ffe9e4fe8f0c0f7ba6ca5b35cf989c76ddb
Aaron Lun <alun@wehi.edu.au>
2017-08-23 15:06:51 +0100

    Supported other GenomicRanges manipulations.

--- R/GRanges-methods.R
+++ R/GRanges-methods.R
@@ -1,10 +1,22 @@
 for (siglist in c("GInteractions", "ContactMatrix")) {
+    # We use regions<- to force reordering if necessary.
     setMethod("trim", siglist, function(x, use.names=TRUE) {
-        out <- trim(regions(x), use.names=use.names)
-        collated <- .collate_GRanges(out)
-        unchecked_regions(x) <- collated$ranges
-        unchecked_anchor1(x) <- collated$indices[[1]][anchor1(x)]
-        unchecked_anchor2(x) <- collated$indices[[1]][anchor2(x)]
+        regions(x) <- trim(regions(x), use.names=use.names)
+        return(x)
+    })
+
+    setMethod("resize", siglist, function(x, width, fix="start", use.names=TRUE, ...) {
+        regions(x) <- resize(regions(x), width=width, fix=fix, use.names=use.names, ...)
+        return(x)
+    })
+
+    setMethod("narrow", siglist, function(x, start=NA, end=NA, width=NA, use.names=TRUE) {
+        regions(x) <- narrow(regions(x), start=start, end=end, width=width, use.names=use.names)
+        return(x)        
+    })  
+
+    setMethod("shift", siglist, function(x, shift=0L, use.names=TRUE) {
+        regions(x) <- shift(regions(x), shift=shift, use.names=use.names)
         return(x)
     })
 }
@@ -19,8 +31,26 @@ setMethod("width", "ContactMatrix", function(x) {
     list(anchor1=w[anchor1(x)], anchor2=w[anchor2(x)])
 })
 
+# Same methods for the interaction set.
+
 setMethod("trim", "InteractionSet", function(x, use.names=TRUE) { 
     interactions(x) <- trim(interactions(x), use.names=use.names) 
     return(x)
 })
+
+setMethod("resize", "InteractionSet", function(x, width, fix="start", use.names=TRUE, ...) {
+    interactions(x) <- resize(interactions(x), width=width, fix=fix, use.names=use.names, ...)
+    return(x)
+})
+
+setMethod("narrow", "InteractionSet", function(x, start=NA, end=NA, width=NA, use.names=TRUE) {
+    interactions(x) <- narrow(interactions(x), start=start, end=end, width=width, use.names=use.names)
+    return(x)        
+})  
+
+setMethod("shift", "InteractionSet", function(x, shift=0L, use.names=TRUE) {
+    interactions(x) <- shift(interactions(x), shift=shift, use.names=use.names)
+    return(x)
+})
+
 setMethod("width", "InteractionSet", function(x) { width(interactions(x)) })
--- tests/testthat/test-grmeth.R
+++ tests/testthat/test-grmeth.R
@@ -12,39 +12,95 @@ Np <- 20
 all.anchor1 <- sample(N, Np)
 all.anchor2 <- sample(N, Np)
 x <- GInteractions(all.anchor1, all.anchor2, all.regions)
-suppressWarnings(seqlengths(x) <- c(chrA=50, chrB=100))
 
-test_that("trimming works for GI, IS objects", {
+test_that("trimming works for all objects", {
+    suppressWarnings(seqlengths(x) <- c(chrA=50, chrB=100))
     x2 <- trim(x)
     ref <- trim(regions(x))
 
     expect_identical(anchors(x2, type="first"), ref[anchors(x, type="first", id=TRUE)])
     expect_identical(anchors(x2, type="second"), ref[anchors(x, type="second", id=TRUE)])
-    
-    suppressWarnings(regions(x2) <- resize(regions(x2), fix="center", width=1000)) 
-    x2 <- trim(x2) # reduces down to one range per chromosome.
-    expect_identical(length(regions(x2)), length(seqlengths(x2)))
+    expect_false(is.unsorted(regions(x2)))
     
     iset <- InteractionSet(matrix(0, nrow=Np, ncol=2), x)
     iset2 <- trim(iset)
     expect_identical(class(iset2), class(iset))
-    expect_identical(interactions(iset2), trim(x))
-})
+    expect_identical(interactions(iset2), x2)
 
-test_that("trimming works for CM objects", {
+    # Now for CM objects.
     y <- inflate(x, "chrA", "chrB")
     y2 <- trim(y)
-    ref <- trim(regions(x))
 
     expect_identical(class(y), class(y2))
     expect_identical(anchors(y2, type="row"), ref[anchors(y, type="row", id=TRUE)])
     expect_identical(anchors(y2, type="column"), ref[anchors(y, type="column", id=TRUE)])
+})
 
-    # Forcing all regions to be the full chromosome, such that copies are eliminated.
-    y3 <- y2
-    suppressWarnings(regions(y3) <- resize(regions(y2), fix="center", width=1000)) 
-    y3 <- trim(y3) 
-    expect_identical(length(regions(y3)), length(seqlengths(y2)))
+test_that("resizing works for all objects", {
+    new.size <- round(runif(N, 10, 50))
+    suppressWarnings(x2 <- resize(x, fix="center", width=new.size))
+    ref <- resize(regions(x), fix="center", width=new.size)
+
+    expect_identical(anchors(x2, type="first"), ref[anchors(x, type="first", id=TRUE)])
+    expect_identical(anchors(x2, type="second"), ref[anchors(x, type="second", id=TRUE)])
+    expect_false(is.unsorted(regions(x2)))
+    
+    iset <- InteractionSet(matrix(0, nrow=Np, ncol=2), x)
+    iset2 <- resize(iset, fix="center", width=new.size)
+    expect_identical(class(iset2), class(iset))
+    expect_identical(interactions(iset2), x2)
+
+    # Now for CM objects.
+    y <- inflate(x, "chrA", "chrB")
+    y2 <- resize(y, fix="center", width=new.size)
+
+    expect_identical(class(y), class(y2))
+    expect_identical(anchors(y2, type="row"), ref[anchors(y, type="row", id=TRUE)])
+    expect_identical(anchors(y2, type="column"), ref[anchors(y, type="column", id=TRUE)])
+})
+
+test_that("narrowing works for all objects", {
+    suppressWarnings(x2 <- narrow(x, start=3))
+    ref <- narrow(regions(x), start=3)
+
+    expect_identical(anchors(x2, type="first"), ref[anchors(x, type="first", id=TRUE)])
+    expect_identical(anchors(x2, type="second"), ref[anchors(x, type="second", id=TRUE)])
+    expect_false(is.unsorted(regions(x2)))
+    
+    iset <- InteractionSet(matrix(0, nrow=Np, ncol=2), x)
+    iset2 <- narrow(iset, start=3)
+    expect_identical(class(iset2), class(iset))
+    expect_identical(interactions(iset2), x2)
+
+    # Now for CM objects.
+    y <- inflate(x, "chrA", "chrB")
+    y2 <- narrow(y, start=3)
+
+    expect_identical(class(y), class(y2))
+    expect_identical(anchors(y2, type="row"), ref[anchors(y, type="row", id=TRUE)])
+    expect_identical(anchors(y2, type="column"), ref[anchors(y, type="column", id=TRUE)])
+})
+
+test_that("shifting works for all objects", {
+    suppressWarnings(x2 <- shift(x, shift=10))
+    ref <- shift(regions(x), shift=10)
+
+    expect_identical(anchors(x2, type="first"), ref[anchors(x, type="first", id=TRUE)])
+    expect_identical(anchors(x2, type="second"), ref[anchors(x, type="second", id=TRUE)])
+    expect_false(is.unsorted(regions(x2)))
+    
+    iset <- InteractionSet(matrix(0, nrow=Np, ncol=2), x)
+    iset2 <- shift(iset, shift=10)
+    expect_identical(class(iset2), class(iset))
+    expect_identical(interactions(iset2), x2)
+
+    # Now for CM objects.
+    y <- inflate(x, "chrA", "chrB")
+    y2 <- shift(y, shift=10)
+
+    expect_identical(class(y), class(y2))
+    expect_identical(anchors(y2, type="row"), ref[anchors(y, type="row", id=TRUE)])
+    expect_identical(anchors(y2, type="column"), ref[anchors(y, type="column", id=TRUE)])
 })
 
 test_that("width calculations work for all objects", {
